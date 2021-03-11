b7e2a9576ad69a1cb4535bbc9c2623be45256636
hagooboy <66807067+hagooboy@users.noreply.github.com>
2021-03-10 22:29:03 -0800

    Added pages

---
+++ gameity-prototype/src/app/stat-board/stat-board.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StatBoardComponent } from './stat-board.component';
+
+describe('StatBoardComponent', () => {
+  let component: StatBoardComponent;
+  let fixture: ComponentFixture<StatBoardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StatBoardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StatBoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
