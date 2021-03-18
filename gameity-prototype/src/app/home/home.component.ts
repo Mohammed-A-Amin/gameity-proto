2a9d6706b394291e5983166679c260bf1e6ee333
unknown <kevin.a8988@gmail.com>
2021-03-18 00:17:30 -0700

    completed descripption for each charity, styled each charity container, fixed navbar

--- gameity-prototype/src/app/home/home.component.ts
+++ gameity-prototype/src/app/home/home.component.ts
@@ -6,17 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
-// $('.navTrigger').click(function () {
-//   $(this).toggleClass('active');
-//   console.log("Clicked menu");
-//   $("#mainListDiv").toggleClass("show_list");
-//   $("#mainListDiv").fadeIn();
-
-// });
