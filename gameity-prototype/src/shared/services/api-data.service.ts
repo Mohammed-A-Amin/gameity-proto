86eeb9552a9ca7a20f62a18a35f07cb29f9675f4
hagooboy <66807067+hagooboy@users.noreply.github.com>
2021-03-25 00:17:12 -0700

    Added to data service

--- gameity-prototype/src/shared/services/api-data.service.ts
+++ gameity-prototype/src/shared/services/api-data.service.ts
@@ -8,12 +8,33 @@ const baseUrl = 'http://localhost:8080';
   providedIn: 'root'
 })
 export class ApiDataService {
-  private listOfJournals = new BehaviorSubject([]);
-  public listOfJournalsObs = this.listOfJournals.asObservable();
+  private listOfUsers = new BehaviorSubject([]);
+  public listOfUsersObs = this.listOfUsers.asObservable();
 
   constructor(public http: HttpClient) { }
 
   createNewContactForm(incomingData: any) {
     return this.http.post(`${baseUrl}/create-contact-form`, incomingData);
   }
+
+  addOneUser(incomingData: any) {
+    return this.http.post(`${baseUrl}/add-one-user`, incomingData);
+  }
+
+  addManyUsers(incomingData: any) {
+    return this.http.post(`${baseUrl}/add-many-users`, incomingData);
+  }
+
+  pullAllUsers(){
+    return this.http.get(`${baseUrl}/get-all-users`)
+  }
+  
+  getAllUsers() {
+    this.pullAllUsers().subscribe((data: any) => {
+      this.listOfUsers.next(data);
+    }, (err) => {
+      console.log(err);
+    })
+  }
+  
 }
